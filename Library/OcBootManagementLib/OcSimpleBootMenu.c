b9bc386cf7d1ac4220f629fd8ff8dd6955f5abb7
n-d-k <ndkmail@icloud.com>
2020-03-01 19:02:27 -0600

    small code change

--- Library/OcBootManagementLib/OcSimpleBootMenu.c
+++ Library/OcBootManagementLib/OcSimpleBootMenu.c
@@ -1144,7 +1144,7 @@ CreateIcon (
   }
   
   if (Icon != NULL) {
-    TmpImage = CreateFilledImage (128, 128, TRUE, &mTransparentPixel);
+    TmpImage = CreateFilledImage (Icon->Width, Icon->Height, TRUE, &mTransparentPixel);
     IsAlpha = Icon->IsAlpha;
     Icon->IsAlpha = FALSE;
     ComposeImage (Icon, TmpImage, 0, 0);
